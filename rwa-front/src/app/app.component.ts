a78e4cc7985fed5f513201b240030931f61ab05f
unknown <radovanovicneda@elfak.rs>
2024-09-15 02:05:12 +0200

    default front

---
+++ rwa-front/src/app/app.component.ts
@@ -0,0 +1,13 @@
+import { Component } from '@angular/core';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+
+@Component({
+  selector: 'app-root',
+  templateUrl: './app.component.html',
+  imports:[FontAwesomeModule],
+  styleUrl: './app.component.scss'
+})
+export class AppComponent {
+  title = 'rwa-front';
+  //kad hocu da dodjaem ikonice iy fontawesome dodajem ovde prvo
+}
---
+++ rwa-front/src/app/app.module.ts
@@ -0,0 +1,28 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { FontAwesomeModule } from '@fortawesome/angular-fontawesome';
+import { StoreModule } from '@ngrx/store';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools' 
+import { EffectsModule } from '@ngrx/effects'
+
+@NgModule({
+  declarations: [
+    AppComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    FontAwesomeModule,
+    StoreModule.forRoot({}),
+    StoreDevtoolsModule.instrument({
+      maxAge:25,
+    }),
+    EffectsModule.forRoot([])
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
